33a497f34fcc2cc8deb81bd5b45e105502bd8a87
Felix Wu <mail@flxwu.com>
2018-12-10 23:05:35 +0100

    add productcard styling

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Box } from 'grommet';
-import styled, { keyframes } from 'styled-components';
+import { Box, Heading, Text } from 'grommet';
+import styled from 'styled-components';
 
 export const ProductImage = props => {
   return (
@@ -10,22 +10,51 @@ export const ProductImage = props => {
   );
 };
 
-const ImageContainer = styled(Box)`
-  width: fit-content;
-  height: fit-content;
-`;
+export const ProductDescription = props => {
+  return (
+    <Box>
+      <Heading level="4">Jodelnde Gurke</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet.
+      </Text>
+    </Box>
+  );
+};
 
-const rotate = keyframes`
-from {
-	transform: rotate(0deg);
+class ProductCard extends React.Component {
+  render() {
+    return (
+      <ProductCardContainer elevation="xsmall" direction="row">
+        <ProductDescription />
+        <ProductImage src={`/assets/${this.props.product}.jpg`} />
+      </ProductCardContainer>
+    );
+  }
 }
 
-to {
-	transform: rotate(360deg);
-}`;
+const ProductCardContainer = styled(Box)`
+  width: 80%;
+  height: fit-content;
+  padding: 15px;
+  margin: 3vh 5vw;
+  border-radius: 5px;
+`;
+
+const ImageContainer = styled(Box)`
+  height: fit-content;
+  width: fit-content;
+  min-width: fit-content;
+  min-height: fit-content;
+`;
 
 const Image = styled.img`
+  display: flex;
   height: 10vmin;
   width: 10vmin;
-  animation: ${rotate} 20s linear infinite;
 `;
+
+export default ProductCard;
