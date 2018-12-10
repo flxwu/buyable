ed3527453e23437dcafb8b1e05e2ee9ab7f3087e
avrecum <avrecum@gmail.com>
2018-12-11 00:53:36 +0100

    added api routes structure

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const router_1 = __importDefault(require("./routes/api/router"));
 const app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(morgan_1.default('dev'));
+app.use('/api', router_1.default);
 app.use(express_1.default.static(__dirname + '/../client/build/'));
 exports.default = app;
 //# sourceMappingURL=app.js.map
