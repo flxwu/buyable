fed382d38e94bdaa45d2317688cb64dd73dfd92e
avrecum <avrecum@gmail.com>
2018-12-11 00:53:36 +0100

    added api routes structure

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
-
+import apiRouter from './routes/api/router';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
+app.use('/api', apiRouter);
 app.use(express.static(__dirname + '/../client/build/'));
 
 export default app;
