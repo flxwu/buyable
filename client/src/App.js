a9462dbe7f2f448397fa2bb45f8d4f201d7f83c2
Felix Wu <mail@felixwu.dev>
2018-12-10 19:23:07 +0100

    add basic grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,71 @@
 import React, { Component } from 'react';
-import { Box } from 'grommet';
+import { Box, Grid, Button } from 'grommet';
+import { Menu } from 'grommet-icons';
+import styled from 'styled-components';
 
 import gurke from './assets/gurke.jpg';
 import { ProductImage } from './components/ProductCard';
 
 class App extends Component {
+  state = { sidebar: true };
   render() {
+    const { sidebar } = this.state;
     return (
-      <Box animation="zoomIn">
-        <ProductImage src={gurke}/>
-      </Box>
+      <Grid
+        className="app-grid"
+        fill rows={['auto', 'flex']}
+        columns={['auto', 'flex']}
+        areas=
+        {[
+          { name: 'header', start: [0, 0], end: [1, 0] },
+          { name: 'sidebar', start: [0, 1], end: [0, 1] },
+          { name: 'main', start: [1, 1], end: [1, 1] }
+        ]}
+        >
+        <Header
+            gridArea="header"
+            direction="row"
+            align="center"
+            justify="between"
+            pad={{ horizontal: "medium", vertical: "small" }}
+          >
+            <Button onClick={() => this.setState({ sidebar: !sidebar })}>
+              <Menu />
+            </Button>
+            Hi Alex!
+          </Header>
+        {sidebar && (
+            <SideBar
+              gridArea="sidebar"
+              width="small"
+              animation={[
+                { type: "fadeIn", duration: 300 },
+                { type: "slideRight", size: "xlarge", duration: 450 }
+              ]}
+            >
+              {["First", "Second", "Third"].map(name => (
+                <Button key={name} href="#" hoverIndicator>
+                  <Box pad={{ horizontal: "medium", vertical: "small" }}>
+                    {name}
+                  </Box>
+                </Button>
+              ))}
+            </SideBar>
+          )}
+        <Box animation="zoomIn" gridArea="main" align="center" justify="center">
+          <ProductImage src={gurke} />
+          <ProductImage src={gurke} />
+        </Box>
+      </Grid>
     );
   }
 }
+
+const Header = styled(Box)`
+  border-bottom: 1px solid grey;
+`;
+
+const SideBar = styled(Box)`
+`;
 
 export default App;
--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -4,11 +4,16 @@ import styled, { keyframes } from 'styled-components';
 
 export const ProductImage = props => {
   return (
-    <Box animation="jiggle">
+    <ImageContainer>
       <Image src={props.src} alt={props.alt} />
-    </Box>
+    </ImageContainer>
   );
 };
+
+const ImageContainer = styled(Box)`
+  width: fit-content;
+  height: fit-content;
+`;
 
 const rotate = keyframes`
 from {
@@ -20,6 +25,7 @@ to {
 }`;
 
 const Image = styled.img`
-  height: 40vmin;
+  height: 10vmin;
+  width: 10vmin;
   animation: ${rotate} 20s linear infinite;
 `;
