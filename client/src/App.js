9bd0a43fdd0439fc8fab719c522c1f88804ccbeb
Felix Wu <mail@felixwu.dev>
2018-12-10 22:47:47 +0100

    adapt grid when sidebar is hidden

--- client/src/App.js
+++ client/src/App.js
@@ -13,46 +13,53 @@ class App extends Component {
     return (
       <Grid
         className="app-grid"
-        fill rows={['auto', 'flex']}
+        fill
+        rows={['auto', 'flex']}
         columns={['auto', 'flex']}
-        areas=
-        {[
-          { name: 'header', start: [0, 0], end: [1, 0] },
-          { name: 'sidebar', start: [0, 1], end: [0, 1] },
-          { name: 'main', start: [1, 1], end: [1, 1] }
-        ]}
-        >
+        areas={
+          sidebar
+            ? [
+                { name: 'header', start: [0, 0], end: [1, 0] },
+                { name: 'sidebar', start: [0, 1], end: [0, 1] },
+                { name: 'main', start: [1, 1], end: [1, 1] }
+              ]
+            : [
+                { name: 'header', start: [0, 0], end: [1, 0] },
+                { name: 'main', start: [0, 1], end: [1, 1] }
+              ]
+        }>
         <Header
-            gridArea="header"
-            direction="row"
-            align="center"
-            justify="between"
-            pad={{ horizontal: "medium", vertical: "small" }}
-          >
-            <Button onClick={() => this.setState({ sidebar: !sidebar })}>
-              <Menu />
-            </Button>
-            Hi Alex!
-          </Header>
+          gridArea="header"
+          direction="row"
+          align="center"
+          justify="between"
+          pad={{ horizontal: 'medium', vertical: 'small' }}>
+          <Button onClick={() => this.setState({ sidebar: !sidebar })}>
+            <Menu />
+          </Button>
+          Hi Alex!
+        </Header>
         {sidebar && (
-            <SideBar
-              gridArea="sidebar"
-              width="small"
-              animation={[
-                { type: "fadeIn", duration: 300 },
-                { type: "slideRight", size: "xlarge", duration: 450 }
-              ]}
-            >
-              {["First", "Second", "Third"].map(name => (
-                <Button key={name} href="#" hoverIndicator>
-                  <Box pad={{ horizontal: "medium", vertical: "small" }}>
-                    {name}
-                  </Box>
-                </Button>
-              ))}
-            </SideBar>
-          )}
-        <Box animation="zoomIn" gridArea="main" align="center" justify="center">
+          <SideBar
+            gridArea="sidebar"
+            width="small"
+            animation={[
+              { type: 'fadeIn', duration: 300 },
+              { type: 'slideRight', size: 'xlarge', duration: 450 }
+            ]}>
+            {['First', 'Second', 'Third'].map(name => (
+              <Button key={name} href="#" hoverIndicator>
+                <Box pad={{ horizontal: 'medium', vertical: 'small' }}>
+                  {name}
+                </Box>
+              </Button>
+            ))}
+          </SideBar>
+        )}
+        <Box
+          gridArea="main"
+          align="center"
+          justify="center">
           <ProductImage src={gurke} />
           <ProductImage src={gurke} />
         </Box>
@@ -65,7 +72,6 @@ const Header = styled(Box)`
   border-bottom: 1px solid grey;
 `;
 
-const SideBar = styled(Box)`
-`;
+const SideBar = styled(Box)``;
 
 export default App;
