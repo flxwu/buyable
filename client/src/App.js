143eec81b23709ae80650ddca2c602f37734a58a
Felix Wu <mail@flxwu.com>
2018-12-11 17:07:10 +0100

    toggle modal on sell product click

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Box } from 'grommet';
 
 import IndexGrid from './components/IndexGrid';
 import Header from './components/header';
 import SideBar from './components/sidebar';
+import NewProductModal from './components/newProductModal';
 import Main from './components/main';
 
-class App extends Component {
-  state = { sidebar: true };
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showSideBar: false, newProductModalOpen: false };
+
+    this.onToggleNewProductModal = this.onToggleNewProductModal.bind(this);
+  }
   render() {
-    const { sidebar } = this.state;
+    const { showSideBar, newProductModalOpen } = this.state;
+
     return (
-      <IndexGrid sidebar={sidebar}>
-        <Header toggleSidebar={() => this.setState({ sidebar: !sidebar })} />
-        {sidebar && <SideBar />}
-        <Main />
-      </IndexGrid>
+      <Box>
+        <IndexGrid showSideBar={showSideBar}>
+          <Header
+            toggleSideBar={() => this.setState({ showSideBar: !showSideBar })}
+            toggleNewProductModal={this.onToggleNewProductModal}
+          />
+          {showSideBar && <SideBar />}
+          <Main />
+        </IndexGrid>
+        {newProductModalOpen && (
+          <NewProductModal
+            onToggleNewProductModal={this.onToggleNewProductModal}
+          />
+        )}
+      </Box>
     );
+  }
+
+  onToggleNewProductModal() {
+    this.setState({ newProductModalOpen: !this.state.newProductModalOpen });
   }
 }
 
