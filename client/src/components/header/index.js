73ac9bb30e062ec2f160c3dbd53671b5e78415f9
Felix Wu <mail@felixwu.dev>
2018-12-11 17:07:10 +0100

    toggle modal on sell product click

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -3,22 +3,31 @@ import styled from 'styled-components';
 import { Box, Button, Text } from 'grommet';
 import { Menu, Camera } from 'grommet-icons';
 
-const Header = props => (
-  <HeaderContainer
-    gridArea="header"
-    direction="row"
-    align="center"
-    justify="between"
-    pad={{ horizontal: 'medium', vertical: 'small' }}>
-    <Button onClick={props.toggleSidebar}>
-      <Menu />
-    </Button>
-    <RightHeader direction="row">
-      <Button icon={<Camera />} label="Sell Product"/>
-      <Text>Hi Alex!</Text>
-    </RightHeader>
-  </HeaderContainer>
-);
+class Header extends React.Component {
+  render() {
+    const { toggleSideBar, toggleNewProductModal } = this.props;
+    return (
+      <HeaderContainer
+        gridArea="header"
+        direction="row"
+        align="center"
+        justify="between"
+        pad={{ horizontal: 'medium', vertical: 'small' }}>
+        <Button onClick={toggleSideBar}>
+          <Menu />
+        </Button>
+        <RightHeader direction="row">
+          <Button
+            icon={<Camera />}
+            label="Sell Product"
+            onClick={toggleNewProductModal}
+          />
+          <Text>Hi Alex!</Text>
+        </RightHeader>
+      </HeaderContainer>
+    );
+  }
+}
 
 const HeaderContainer = styled(Box)`
   border-bottom: 1px solid grey;
