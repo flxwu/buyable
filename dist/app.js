17649e1c503c03151fe240f9b2c2a34ad0409db5
Felix Wu <mail@flxwu.com>
2018-12-11 10:02:25 +0100

    add mlab and mongo session store

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
+const express_session_1 = __importDefault(require("express-session"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const app = express_1.default();
+const MongoStore = connect_mongo_1.default(express_session_1.default);
+// Session Handling
+const mongoURL = `mongodb://flxwu:${process.env.MLAB_PASSWORD}@ds016108.mlab.com:16108/buyable-dev`;
+app.use(express_session_1.default({
+    secret: 'foo',
+    store: new MongoStore({
+        url: mongoURL
+    }),
+    resave: true,
+    saveUninitialized: true
+}));
+// MongoDB
+mongoose_1.default.connect(mongoURL);
+const db = mongoose_1.default.connection;
+db.on('error', () => console.error('Error connecting to MLab MongoDB'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(morgan_1.default('dev'));
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,32 @@
 import express from 'express';
 import morgan from 'morgan';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import MongoConnect from 'connect-mongo';
 
 const app = express();
+
+const MongoStore = MongoConnect(session);
+
+// Session Handling
+const mongoURL = `mongodb://flxwu:${
+  process.env.MLAB_PASSWORD
+}@ds016108.mlab.com:16108/buyable-dev`;
+app.use(
+  session({
+    secret: 'foo',
+    store: new MongoStore({
+      url: mongoURL
+    }),
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// MongoDB
+mongoose.connect(mongoURL);
+const db = mongoose.connection;
+db.on('error', () => console.error('Error connecting to MLab MongoDB'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
