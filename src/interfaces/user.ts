4df4a734baf221715376e2ae58a6511b0674d271
avrecum <avrecum@gmail.com>
2018-12-13 00:37:06 +0100

    update models and schemas

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,7 +1,14 @@
-import { IItemReference } from './reference';
+import { IItemReference, IGroupReference } from './reference';
 
 export interface IUser {
-	_id: string,
-	name?: string,
-	items?: Array<IItemReference>
+    _id: string;
+    username: string;
+    password: string;
+    email: string;
+    forename?: string;
+    surname?: string;
+    items?: Array<IItemReference>;
+    groups?: Array<IGroupReference>;
+    ownedGroups?: Array<IGroupReference>;
+
 }
