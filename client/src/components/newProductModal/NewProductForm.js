0056164d943df4d1f7b643d68a88cf12c24a16f5
Felix Wu <mail@flxwu.com>
2018-12-12 21:03:49 +0100

    add product, price, image input fields

--- client/src/components/newProductModal/NewProductForm.js
+++ client/src/components/newProductModal/NewProductForm.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import { Box } from 'grommet';
 import styled from 'styled-components';
 
-import TextInputField from './TextInputField';
+import TextInputField from '../form/TextInputField';
+import ImageUploadField from '../form/ImageUploadField';
 
 class NewProductForm extends Component {
   render() {
     return (
       <FormContainer>
-        <TextInputField label="Product" placeholder=""/>
-
+        <TextInputField label="Product" placeholder="e.g. yodeling cucumber"/>
+        <TextInputField label="Price" placeholder="e.g. 300" />
+        <ImageUploadField label="Product Image" />
       </FormContainer>
     );
   }
 }
 
 const FormContainer = styled(Box)`
-	min-width: 80%
+	min-width: 80%;
+  max-width: 80%;
+  margin: 15px 0;
 `;
 
 export default NewProductForm;
