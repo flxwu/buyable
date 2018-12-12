1bff599297ca57b207fe74867bb3252e154597db
Felix Wu <mail@flxwu.com>
2018-12-12 21:03:36 +0100

    add text input field component

---
+++ client/src/components/form/TextInputField.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FormField, TextInput } from 'grommet';
+
+class TextInputField extends React.Component {
+	state = { value: '' };
+
+  render() {
+    const { placeholder } = this.props;
+    const { value } = this.state;
+    return (
+      <FormField {...this.props}>
+        <TextInput
+          id="text-input"
+          placeholder={placeholder}
+          value={value}
+          onChange={this.onTextChange}
+        />
+      </FormField>
+    );
+  }
+
+  onTextChange = evt => this.setState({ value: evt.target.value });
+}
+
+export default TextInputField;
