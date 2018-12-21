cc288d3404d83ab59f9a58881a5becafb79dc9ea
Felix Wu <mail@flxwu.com>
2018-12-21 18:14:04 +0100

    feat(api, auth): add /check endpoint, return if user is logged in - fix #13

--- dist/routes/api/authRouter.js
+++ dist/routes/api/authRouter.js
@@ -2,6 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = express_1.Router();
+router.get('/check', (req, res) => {
+    const user = req.session.user;
+    if (user && user._id) {
+        res.status(200).json({ loggedIn: true });
+    }
+    else {
+        res.status(200).json({ loggedIn: false });
+    }
+});
 router.use('/login', (req, res) => {
     //TODO: store profile into session
 });
--- src/routes/api/authRouter.ts
+++ src/routes/api/authRouter.ts
@@ -2,14 +2,24 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/check', (req: any, res: any) => {
+	const user = req.session.user;
+
+	if (user && user._id) {
+		res.status(200).json({ loggedIn: true })
+	} else {
+		res.status(200).json({ loggedIn: false })
+	}
+});
+
 router.use('/login', (req: any, res: any) => {
-	//TODO: store profile into session
+  //TODO: store profile into session
 });
 
 router.use('/logout', (req: any, res: any) => {
-	req.session.profile = {};
-	res.status(200);
-	res.send('Successfully logged out');
+  req.session.profile = {};
+  res.status(200);
+  res.send('Successfully logged out');
 });
 
 export default router;
