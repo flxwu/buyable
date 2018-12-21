5acfdfbb2c5c01fb877504ee58d37aa200d4b6ab
Felix Wu <mail@felixwu.dev>
2018-12-21 21:49:21 +0100

    feat(api, schemas, user): add verifyPassword method

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,5 +1,6 @@
 import { Document, Schema, Model, model } from 'mongoose';
 import { IUser } from '../interfaces/user';
+import bcrypt from 'bcrypt';
 
 export interface IUserModel extends IUser, Document {
   _id: any | string;
@@ -23,14 +24,23 @@ export const UserSchema: Schema = new Schema({
   items: Array,
   groups: Array,
   ownedGroups: Array,
+  createdAt: String
 });
 
 UserSchema.pre('save', function(next: any) {
   const now = new Date();
   if (!this.createdAt) {
-    this.createdAt = now;
+    this.createdAt = now.getTime();
   }
   next();
 });
+
+UserSchema.methods.verifyPassword = async function verifyPassword(password: String) {
+  const matches = await bcrypt.compare(password, this.password);
+  if (matches) {
+    return true;
+  }
+  return false;
+}
 
 export const UserModel: Model<IUserModel> = model<IUserModel>('User', UserSchema);
