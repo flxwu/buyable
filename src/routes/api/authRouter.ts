981708c99f9c188ef2daa29b3e46531b60170330
Felix Wu <mail@flxwu.com>
2018-12-21 21:49:52 +0100

    feat(api, auth): implement login/logout using passport

--- src/routes/api/authRouter.ts
+++ src/routes/api/authRouter.ts
@@ -1,24 +1,30 @@
 import { Router } from 'express';
+import { validateEmail } from '../../helpers/api';
+import passport from 'passport';
 
 const router = Router();
 
 router.get('/check', (req: any, res: any) => {
-	const user = req.session.user;
+  const user = req.user;
 
-	if (user && user._id) {
-		res.status(200).json({ loggedIn: true })
-	} else {
-		res.status(200).json({ loggedIn: false })
-	}
+  if (user && user._id) {
+    res.status(200).json({ loggedIn: true });
+  } else {
+    res.status(200).json({ loggedIn: false });
+  }
 });
 
-router.use('/login', (req: any, res: any) => {
-  //TODO: store profile into session
-});
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  function(req, res) {
+		res.status(200).send(req.user);
+  }
+);
 
-router.use('/logout', (req: any, res: any) => {
-  req.session.profile = {};
-  res.status(200);
+router.get('/logout', (req: any, res: any) => {
+	req.logout();
+  res.redirect('/');
   res.send('Successfully logged out');
 });
 
