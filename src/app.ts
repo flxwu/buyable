cdfb36c1904996f809f5ce0529087d7d8978a61d
Felix Wu <mail@flxwu.com>
2018-12-21 21:47:31 +0100

    feat(api, auth): local login strategy using passport

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import morgan from 'morgan';
 import session from 'express-session';
 import mongoose from 'mongoose';
 import MongoConnect from 'connect-mongo';
+import passport from 'passport';
+import passportLocal from 'passport-local';
 
+import { UserModel } from './schemas/user';
 import apiRouter from './routes/api/router';
 
 const app = express();
@@ -25,10 +28,41 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
 // MongoDB
 mongoose.connect(mongoURL);
 const db = mongoose.connection;
 db.on('error', () => console.error('Error connecting to MLab MongoDB'));
+
+const LocalStrategy = passportLocal.Strategy;
+passport.serializeUser<any, any>((user, done) => {
+  done(undefined, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  UserModel.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    UserModel.findOne({ username }, async function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      const pwCorrect = await user.verifyPassword(password);
+      if (!pwCorrect) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,14 +1,14 @@
 import { IItemReference, IGroupReference } from './reference';
 
 export interface IUser {
-    _id: string;
-    username: string;
-    password: string;
-    email: string;
-    forename?: string;
-    surname?: string;
-    items?: Array<IItemReference>;
-    groups?: Array<IGroupReference>;
-    ownedGroups?: Array<IGroupReference>;
-
+  _id: string;
+  username: string;
+  password: string;
+  email: string;
+  forename?: string;
+  surname?: string;
+  items?: Array<IItemReference>;
+  groups?: Array<IGroupReference>;
+  ownedGroups?: Array<IGroupReference>;
+  verifyPassword: Function;
 }
