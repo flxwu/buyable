075069eb83177e607eb5cbd8964d00563c5a4619
Felix Wu <mail@felixwu.dev>
2018-12-22 15:02:01 +0100

    style(client, auth): design alignment buttons

--- client/src/components/authModal/LoginForm.js
+++ client/src/components/authModal/LoginForm.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import {
-  Box,
-  Heading,
-  Button
-} from 'grommet';
+import { Box, Heading, Button } from 'grommet';
 import TextInputField from '../form/TextInputField';
 class LoginForm extends React.Component {
   //TODO: Add login handler and onChange listeners
   render() {
-    return ( <FormContainer>
-      <Heading textAlign = "center"
-      size = "small" >
-      Login </Heading> <TextInputField 
-      label = "Username"
-      placeholder = "yodelingcucumber" />
-      <TextInputField 
-      label = "Password"
-      type = "password"
-      placeholder = "Your Password" />
-      <Button primary label = "Login" hoverIndicator="accent-1" margin="medium" > </Button>  </FormContainer >
+    return (
+      <FormContainer>
+        <Heading textAlign="center" size="small">
+          Login{' '}
+        </Heading>{' '}
+        <TextInputField label="Username" placeholder="yodelingcucumber" />
+        <TextInputField
+          label="Password"
+          type="password"
+          placeholder="Your Password"
+        />
+        <Button label="Login" margin="medium" />{' '}
+      </FormContainer>
     );
   }
 }
 
-const FormContainer = styled(Box)
-`
+const FormContainer = styled(Box)`
   min-width: 80%;
   max-width: 80%;
   margin: 15px 0;
