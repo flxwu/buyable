7f9d07c2842fa95549a04f8e68b88dce5d568037
Felix Wu <mail@flxwu.com>
2018-12-23 21:43:11 +0100

    feat(client): add top-level Redux and React-Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import Profile from './Profile';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </Router>
+  </Provider>,
+  document.querySelector('#root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
