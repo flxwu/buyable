7c628b299c8b9a2144a0ce13471caaa30dc9db90
Felix Wu <mail@flxwu.com>
2018-12-23 16:23:05 +0100

    feat(client, auth): submit on enter

--- client/src/components/authModal/LoginForm.js
+++ client/src/components/authModal/LoginForm.js
@@ -5,58 +5,24 @@ import TextInputField from '../form/TextInputField';
 import axios from 'axios';
 class LoginForm extends React.Component {
   //TODO: Add login handler and onChange listeners
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {usernameField: "", passwordField: ""}
-    const {onUserStateChange, onToggleAuthModal} = this.props;
-    this.onLoginSubmit = async ()=> {
-      try{
-        const result = await axios.post('/api/auth/login',{
-          username: this.state.usernameField,
-          password: this.state.passwordField
-        });
-        const user = result.data;
-        onToggleAuthModal();
-        onUserStateChange(user);
-        
-      }catch(err){
-        if(err.response){
-          const {status} = err.response;
-          if(status === 401){
-            alert("wrong username or password")
-          }
-          if(status === 400){
-            alert("provide both username and password")
-          }
-          if(status === 500){
-            alert("internal server error")
-          }
-        }
-        
-      }
-      
-    }
-    this.onUsernameChange = (event) => {
-      this.setState({usernameField: event.target.value.substr(0,100)})
-      
-    }
-    this.onPasswordChange = (event) => {
-      this.setState({passwordField: event.target.value.substr(0,72)})
-    }
+    this.state = { usernameField: '', passwordField: '' };
   }
-  
+
   render() {
     return (
       <FormContainer>
         <Heading textAlign="center" size="small">
           Login
         </Heading>
-        <TextInputField 
-        label="Username" 
-        id="usernameField"
-        placeholder="yodelingcucumber" 
-        value={this.state.usernameField} 
-        onChange={this.onUsernameChange}/>
+        <TextInputField
+          label="Username"
+          id="usernameField"
+          placeholder="yodelingcucumber"
+          value={this.state.usernameField}
+          onChange={this.onUsernameChange}
+        />
         <TextInputField
           label="Password"
           type="password"
@@ -64,11 +30,53 @@ class LoginForm extends React.Component {
           placeholder="Your Password"
           value={this.state.passwordField}
           onChange={this.onPasswordChange}
+          onKeyDown={this.listenForEnter}
         />
-        <Button label="Login" margin="medium" onClick={this.onLoginSubmit}/>
+        <Button label="Login" margin="medium" onClick={this.onLoginSubmit} />
       </FormContainer>
     );
   }
+
+  onLoginSubmit = async () => {
+    const { onUserStateChange, onToggleAuthModal } = this.props;
+
+    try {
+      const result = await axios.post('/api/auth/login', {
+        username: this.state.usernameField,
+        password: this.state.passwordField
+      });
+      const user = result.data;
+      onToggleAuthModal();
+      onUserStateChange(user);
+    } catch (err) {
+      if (err.response) {
+        const { status } = err.response;
+        if (status === 401) {
+          alert('wrong username or password');
+        }
+        if (status === 400) {
+          alert('provide both username and password');
+        }
+        if (status === 500) {
+          alert('internal server error');
+        }
+      }
+    }
+  };
+
+  onPasswordChange = event => {
+    this.setState({ passwordField: event.target.value.substr(0, 72) });
+  };
+
+  onUsernameChange = event => {
+    this.setState({ usernameField: event.target.value.substr(0, 100) });
+  };
+
+  listenForEnter = event => {
+    if (event.keyCode === 13) {
+      this.onLoginSubmit();
+    }
+  };
 }
 
 const FormContainer = styled(Box)`
@@ -78,4 +86,3 @@ const FormContainer = styled(Box)`
 `;
 
 export default LoginForm;
-
