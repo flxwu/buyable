62ddb9feda95e83729dcedd0cfcc9ceade07211e
avrecum <avrecum@gmail.com>
2018-12-25 17:34:56 +0100

    delete router from index

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { registerObserver } from 'react-perf-devtool';
+import React from "react";
+import ReactDOM from "react-dom";
+import { registerObserver } from "react-perf-devtool";
 
-import './index.css';
-import App from './App';
-import Profile from './Profile';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 window.observer = registerObserver();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <div>
-        <Route exact path="/" component={App} />
-        <Route path="/profile" component={Profile} />
-      </div>
-    </Router>
+    <App />
   </Provider>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
 
 // If you want your app to work offline and load faster, you can change
