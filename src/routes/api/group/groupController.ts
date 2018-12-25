3d59129d0377c87f941cc647a60f506d03a0a391
Felix Wu <mail@flxwu.com>
2018-12-26 00:08:58 +0100

    feat(api, group): implement /new

--- src/routes/api/group/groupController.ts
+++ src/routes/api/group/groupController.ts
@@ -1,16 +1,70 @@
+import { GroupModel, IGroupModel } from '../../../schemas/group';
+
 interface IController {
-    newPOST: Function;
+  newPOST: Function;
 }
 
 class Controller<IController> {
-    constructor() {}
-    public newPOST(req: any, res: any, next: any): any {
+  public async newPOST(req: any, res: any, next: any): Promise<any> {
+    const {
+      name,
+      description,
+      urlSuffix,
+      permissions,
+      settings,
+      pictureURL,
+      items
+    } = req.body;
+    const owner = { referenceId: req.user._id };
+    const users = [owner];
+    // TODO: Validation
+    // TODO: Upload image to s3 and create url
+    const duplicate = await GroupModel.findOne({ urlSuffix }).exec();
+    if (!duplicate) {
+      try {
+        const newGroup: IGroupModel = new GroupModel({
+          name,
+          description,
+          urlSuffix,
+          permissions,
+          settings,
+          pictureURL,
+          owner,
+          users,
+          items
+        });
+        await newGroup.save();
+        res.status(201).json({
+          group: {
+            name,
+            description,
+            urlSuffix,
+            permissions,
+            settings,
+            pictureURL,
+            owner,
+            users,
+            items
+          }
+        });
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    } else {
+      res.status(500).json({ error: 'Group already exists' });
+    }
+  }
 
+  public async get(req: any, res: any, next: any): Promise<any> {
+    res.status(200);
+    const { _id } = req.query;
+    try {
+      const group = await GroupModel.findById(_id).exec();
+      res.send(group);
+    } catch (err) {
+      res.status(404).json({ error: err.message });
     }
-    public get(req: any, res: any, next: any): any {
-        res.status(200);
-        res.json({group: 'test'});
-    }
+  }
 }
 
 export default Controller;
