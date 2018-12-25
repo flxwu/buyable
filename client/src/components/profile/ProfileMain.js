e54e7f78e6a63da35c60ea9d51f05dd59c44c4f9
avrecum <avrecum@gmail.com>
2018-12-25 20:14:47 +0100

    connect ProfileMain to redux

--- client/src/components/profile/ProfileMain.js
+++ client/src/components/profile/ProfileMain.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { getCurrentUser, isLoggedIn } from "../../redux/selectors";
+import {
+  deleteUser,
+  addUser,
+  checkUserAuthenticated
+} from "../../redux/actions/user";
 class ProfileMain extends React.Component {
   render() {
-    return <div>Here you jerk off to your profile pic</div>;
+    return <div>{JSON.stringify(this.props.user)}</div>;
   }
 }
 
-export default ProfileMain;
+const mapStateToProps = state => {
+  return { user: getCurrentUser(state), loggedIn: isLoggedIn(state) };
+};
+const mapDispatchToProps = {
+  authCheck: checkUserAuthenticated
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfileMain);
