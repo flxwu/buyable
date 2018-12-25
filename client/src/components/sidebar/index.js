51809a084df754933118bf9a484023005891373f
avrecum <avrecum@gmail.com>
2018-12-25 17:26:44 +0100

    add react router to app

--- client/src/components/sidebar/index.js
+++ client/src/components/sidebar/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Box, Button } from 'grommet';
+import React from "react";
+import styled from "styled-components";
+import { Box, Button } from "grommet";
 
 const SideBar = props => (
   <SideBarContainer
     gridArea="sidebar"
     width="small"
     animation={[
-      { type: 'fadeIn', duration: 300 },
-      { type: 'slideRight', size: 'xlarge', duration: 450 }
-    ]}>
-    {['First', 'Second', 'Third'].map(name => (
-      <Button key={name} href="#" hoverIndicator>
-        <Box pad={{ horizontal: 'medium', vertical: 'small' }}>{name}</Box>
+      { type: "fadeIn", duration: 300 },
+      { type: "slideRight", size: "xlarge", duration: 450 }
+    ]}
+  >
+    {["timeline", "groups", "items"].map(name => (
+      <Button key={name} href={name} hoverIndicator>
+        <Box pad={{ horizontal: "medium", vertical: "small" }}>{name}</Box>
       </Button>
     ))}
   </SideBarContainer>
