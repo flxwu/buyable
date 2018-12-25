53bbfd366fc3b5683015075ecbc895e3bc94b5fc
avrecum <avrecum@gmail.com>
2018-12-25 21:09:59 +0100

    profile design change

--- client/src/components/profile/ProfileMain.js
+++ client/src/components/profile/ProfileMain.js
@@ -1,14 +1,51 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getCurrentUser, isLoggedIn } from "../../redux/selectors";
-import {
-  deleteUser,
-  addUser,
-  checkUserAuthenticated
-} from "../../redux/actions/user";
+import { updateUser, checkUserAuthenticated } from "../../redux/actions/user";
+import { Text, TextInput, Heading, Box, Button } from "grommet";
+import TextInputField from "../form/TextInputField";
 class ProfileMain extends React.Component {
+  constructor(props) {
+    super(props);
+    const { user } = props;
+    this.state = {
+      usernameField: user ? user.username : "",
+      emailField: user ? user.email : ""
+    };
+  }
+  onUsernameFieldChange = e => {
+    this.setState({ usernameField: e.target.value });
+  };
+  onEmailFieldChange = e => {
+    this.setState({ emailField: e.target.value });
+  };
+  onUpdateUser = () => {
+    //updateUserInfo(user)
+    console.log("called");
+  };
   render() {
-    return <div>{JSON.stringify(this.props.user)}</div>;
+    const { user } = this.props;
+    const { usernameField, emailField } = this.state;
+    return (
+      user && (
+        <Box fill width="full">
+          <Heading alignSelf="start">Profile</Heading>
+          <Text alignSelf="start">Username</Text>
+          <TextInputField
+            value={usernameField || user.username}
+            alignSelf="start"
+            onChange={this.onUsernameFieldChange}
+          />
+          <Text alignSelf="start">Email</Text>
+          <TextInputField
+            value={emailField || user.email}
+            alignSelf="start"
+            onChange={this.onEmailFieldChange}
+          />
+          <a onClick={this.onUpdateUser}>Save</a>
+        </Box>
+      )
+    );
   }
 }
 
@@ -16,6 +53,7 @@ const mapStateToProps = state => {
   return { user: getCurrentUser(state), loggedIn: isLoggedIn(state) };
 };
 const mapDispatchToProps = {
+  updateUserInfo: updateUser,
   authCheck: checkUserAuthenticated
 };
 
