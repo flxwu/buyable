4c4cce554481394513b8523e139e84d2b740b854
Felix Wu <mail@felixwu.dev>
2018-12-25 18:06:13 +0100

    fix(client, grid): fix rows&columns with sidebar

--- client/src/components/IndexGrid.js
+++ client/src/components/IndexGrid.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Grid } from 'grommet';
 
 class IndexGrid extends Component {
-	render () {
+  render() {
     const { showSideBar } = this.props;
-		return (
-			<Grid
-			className="app-grid"
-			fill
-			rows={!showSideBar ? ['auto', 'auto'] : ['auto', 'auto', 'auto']}
-			columns={!showSideBar ? ['auto', 'auto'] : ['auto', 'auto', 'auto']}
-			areas={
-				showSideBar
-					? [
-							{ name: 'header', start: [0, 0], end: [1, 0] },
-							{ name: 'sidebar', start: [0, 1], end: [0, 1] },
-							{ name: 'main', start: [1, 1], end: [1, 1] }
-						]
-					: [
-							{ name: 'header', start: [0, 0], end: [1, 0] },
-							{ name: 'main', start: [0, 1], end: [1, 1] }
-						]
-			}>
-				{this.props.children}
-			</Grid>
-		)
-	}
+    return (
+      <Grid
+        className="app-grid"
+        fill
+        rows={['auto', 'auto']}
+        columns={showSideBar ? ['10vw', 'auto'] : ['auto']}
+        areas={
+          showSideBar
+            ? [
+                { name: 'header', start: [0, 0], end: [1, 0] },
+                { name: 'sidebar', start: [0, 1], end: [0, 1] },
+                { name: 'main', start: [1, 1], end: [1, 1] }
+              ]
+            : [
+                { name: 'header', start: [0, 0], end: [1, 0] },
+                { name: 'main', start: [0, 1], end: [1, 1] }
+              ]
+        }>
+        {this.props.children}
+      </Grid>
+    );
+  }
 }
 
-export default IndexGrid
+export default IndexGrid;
