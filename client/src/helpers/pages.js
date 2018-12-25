8537b7c450998e46011fa7e63f55e4d2bd4e51a1
Felix Wu <mail@flxwu.com>
2018-12-25 21:01:54 +0100

    feat(client, routes): add helper function for access restriction to certain pages

--- client/src/helpers/pages.js
+++ client/src/helpers/pages.js
@@ -1,5 +1,8 @@
 export const sidebarPages = [
-  { name: "Timeline", url: "/" },
-  { name: "Groups", url: "/groups" },
-  { name: "Items", url: "/items" }
+  { name: 'Timeline', url: '/' },
+  { name: 'Groups', url: '/groups' },
+  { name: 'Items', url: '/items' }
 ];
+
+export const checkForRestrictedPage = (routePage, fallback, condition) =>
+  condition ? routePage : fallback;
