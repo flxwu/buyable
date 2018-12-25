5dab54c10d74d0f4eef0e348ab4dfac0fa7762e7
avrecum <avrecum@gmail.com>
2018-12-25 01:58:44 +0100

    feat(frontend, login): save user to cookie upon register

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import './index.css';
-import App from './App';
-import Profile from './Profile';
-import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Profile from "./Profile";
+import * as serviceWorker from "./serviceWorker";
+import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -18,7 +17,7 @@ ReactDOM.render(
       </div>
     </Router>
   </Provider>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
 
 // If you want your app to work offline and load faster, you can change
