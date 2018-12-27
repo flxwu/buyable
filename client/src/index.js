692b09bf50166072242b6482c947d3e9e9d18c85
Alexander von Recum <37403595+avrecum@users.noreply.github.com>
2018-12-27 22:30:02 +0100

    Add redux-persist for persisting store and remembering user (#40)
    redux persistence should work now

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { PersistGate } from 'redux-persist/es/integration/react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 
 window.observer = registerObserver();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>,
   document.querySelector('#root')
 );
