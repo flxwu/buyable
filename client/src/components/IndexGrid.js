4aca346cdd2c49d713163dd7625698a4205d4c15
Felix Wu <mail@flxwu.com>
2018-12-27 22:09:29 +0100

    refactor(client): simplify grid,main,profile to functional components

--- client/src/components/IndexGrid.js
+++ client/src/components/IndexGrid.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid } from 'grommet';
 
-class IndexGrid extends Component {
-  render() {
-    const { showSideBar } = this.props;
-    return (
-      <Grid
-        className="app-grid"
-        fill
-        rows={['auto', 'auto']}
-        columns={showSideBar ? ['10vw', 'auto'] : ['auto']}
-        areas={
-          showSideBar
-            ? [
-                { name: 'header', start: [0, 0], end: [1, 0] },
-                { name: 'sidebar', start: [0, 1], end: [0, 1] },
-                { name: 'main', start: [1, 1], end: [1, 1] }
-              ]
-            : [
-                { name: 'header', start: [0, 0], end: [1, 0] },
-                { name: 'main', start: [0, 1], end: [1, 1] }
-              ]
-        }>
-        {this.props.children}
-      </Grid>
-    );
-  }
-}
+const IndexGrid = props => (
+  <Grid
+    className="app-grid"
+    fill
+    rows={['auto', 'auto']}
+    columns={props.showSideBar ? ['10vw', 'auto'] : ['auto']}
+    areas={
+      props.showSideBar
+        ? [
+            { name: 'header', start: [0, 0], end: [1, 0] },
+            { name: 'sidebar', start: [0, 1], end: [0, 1] },
+            { name: 'main', start: [1, 1], end: [1, 1] }
+          ]
+        : [
+            { name: 'header', start: [0, 0], end: [1, 0] },
+            { name: 'main', start: [0, 1], end: [1, 1] }
+          ]
+    }>
+    {props.children}
+  </Grid>
+);
 
 export default IndexGrid;
