4f8df91efcdab2092f27fea0a67738b7724ad73f
Felix Wu <mail@flxwu.com>
2018-12-27 22:01:58 +0100

    refactor(client, auth): rewrite forms using hooks

--- client/src/components/authModal/LoginForm.js
+++ client/src/components/authModal/LoginForm.js
@@ -1,96 +1,92 @@
-import React from "react";
-import axios from "axios";
-import styled from "styled-components";
-import { Box, Heading, Button, Text } from "grommet";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Cookie from 'js-cookie';
+import styled from 'styled-components';
+import { Box, Heading, Button, Text } from 'grommet';
 
-import TextInputField from "../form/TextInputField";
+import TextInputField from '../form/TextInputField';
 
-import { connect } from "react-redux";
-import { addUser } from "../../redux/actions/user";
-import { toggleModal } from "../../redux/actions/modals";
-import Cookie from "js-cookie";
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { usernameField: "", passwordField: "", apiStatusCode: 200 };
-  }
+import { connect } from 'react-redux';
+import { addUser } from '../../redux/actions/user';
+import { toggleModal } from '../../redux/actions/modals';
 
-  render() {
-    return (
-      <FormContainer>
-        <Heading textAlign="center" size="small">
-          Login
-        </Heading>
-        <TextInputField
-          label="Username or email"
-          id="usernameField"
-          placeholder="yodelingcucumber"
-          value={this.state.usernameField}
-          onChange={this.onUsernameChange}
-        />
-        <TextInputField
-          label="Password"
-          type="password"
-          id="passwordField"
-          placeholder="Your Password"
-          value={this.state.passwordField}
-          onChange={this.onPasswordChange}
-          onKeyDown={this.listenForEnter}
-        />
-        <Button label="Login" margin="medium" onClick={this.onLoginSubmit} />
-        {this.state.apiStatusCode === 400 && (
-          <Text color="status-critical" textAlign="center">
-            Provide both username and password
-          </Text>
-        )}
-        {this.state.apiStatusCode === 401 && (
-          <Text color="status-critical" textAlign="center">
-            Wrong username or password
-          </Text>
-        )}
-        {this.state.apiStatusCode === 500 && (
-          <Text color="status-critical" textAlign="center">
-            Internal server error
-          </Text>
-        )}
-      </FormContainer>
-    );
-  }
+const LoginForm = props => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [apiStatusCode, setApiStatusCode] = useState(200);
 
-  onLoginSubmit = async () => {
-    const { toggleModal } = this.props;
-
+  const onLoginSubmit = async () => {
     try {
-      const result = await axios.post("/api/auth/login", {
-        username: this.state.usernameField,
-        password: this.state.passwordField
+      const result = await axios.post('/api/auth/login', {
+        username,
+        password
       });
       const user = result.data;
-      toggleModal(null);
-      this.props.addUser(user);
-      Cookie.set("user", user);
+      props.toggleModal(null);
+      props.addUser(user);
+      Cookie.set('user', user);
     } catch (err) {
       if (err.response) {
         const { status } = err.response;
-        this.setState({ apiStatusCode: status });
+        setApiStatusCode(status);
       }
     }
   };
 
-  onPasswordChange = event => {
-    this.setState({ passwordField: event.target.value.substr(0, 72) });
+  const onPasswordChange = event => {
+    setPassword(event.target.value.substr(0, 72));
   };
 
-  onUsernameChange = event => {
-    this.setState({ usernameField: event.target.value.substr(0, 100) });
+  const onUsernameChange = event => {
+    setUsername(event.target.value.substr(0, 100));
   };
 
-  listenForEnter = event => {
+  const listenForEnter = event => {
     if (event.keyCode === 13) {
-      this.onLoginSubmit();
+      onLoginSubmit();
     }
   };
-}
+
+  return (
+    <FormContainer>
+      <Heading textAlign="center" size="small">
+        Login
+      </Heading>
+      <TextInputField
+        label="Username or email"
+        id="usernameField"
+        placeholder="yodelingcucumber"
+        value={username}
+        onChange={onUsernameChange}
+      />
+      <TextInputField
+        label="Password"
+        type="password"
+        id="passwordField"
+        placeholder="Your Password"
+        value={password}
+        onChange={onPasswordChange}
+        onKeyDown={listenForEnter}
+      />
+      <Button label="Login" margin="medium" onClick={onLoginSubmit} />
+      {apiStatusCode === 400 && (
+        <Text color="status-critical" textAlign="center">
+          Provide both username and password
+        </Text>
+      )}
+      {apiStatusCode === 401 && (
+        <Text color="status-critical" textAlign="center">
+          Wrong username or password
+        </Text>
+      )}
+      {apiStatusCode === 500 && (
+        <Text color="status-critical" textAlign="center">
+          Internal server error
+        </Text>
+      )}
+    </FormContainer>
+  );
+};
 
 const FormContainer = styled(Box)`
   min-width: 80%;
