b569f150ea53bf0f4a3148db38553b8a4bc212d1
Felix Wu <mail@felixwu.dev>
2018-12-27 22:01:34 +0100

    refactor(client, form): rewrite input fields using react hooks

--- client/src/components/form/ImageUploadField.js
+++ client/src/components/form/ImageUploadField.js
@@ -4,17 +4,11 @@ import styled from 'styled-components';
 
 import FileInputField from './FileInputField';
 
-class ImageUploadField extends React.Component {
-  render() {
-    return (
-      <BorderlessFormField {...this.props}>
-        <FileInputField label={this.props.label}/>
-      </BorderlessFormField>
-    );
-  }
-
-  onTextChange = evt => this.setState({ value: evt.target.value });
-}
+const ImageUploadField = ({ ...props }) => (
+  <BorderlessFormField {...props}>
+    <FileInputField label={props.label} />
+  </BorderlessFormField>
+);
 
 const BorderlessFormField = styled(FormField)`
   > * {
