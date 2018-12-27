1a15a3e8d6c1ee23d6b1d426d7681323c939861b
Alexander von Recum <37403595+avrecum@users.noreply.github.com>
2018-12-27 22:30:02 +0100

    Add redux-persist for persisting store and remembering user (#40)
    redux persistence should work now

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,14 +4,25 @@ import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
 import apiSaga from './sagas';
 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
 const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(sagaMiddleware))
 );
 sagaMiddleware.run(apiSaga);
 
+export const persistor = persistStore(store);
 export default store;
