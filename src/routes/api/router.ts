b56fc949d1fbb3ba37f3cb219be8eb999f9c48e4
Felix Wu <mail@felixwu.dev>
2019-01-09 19:09:19 +0100

    feat: add experimentalRouter

--- src/routes/api/router.ts
+++ src/routes/api/router.ts
@@ -4,12 +4,17 @@ import itemRouter from './item/itemRouter';
 import userRouter from './user/userRouter';
 import groupRouter from './group/groupRouter';
 import authRouter from './authRouter';
+import experimentalRouter from './experimentalRouter';
+
 const router: Router = Router();
 
 router.use('/item', itemRouter);
 router.use('/user', userRouter);
 router.use('/group', groupRouter);
 router.use('/auth', authRouter);
+
+router.use('/beta', experimentalRouter);
+
 router.use('/', (req: any, res: any) => {
   res.status(400);
   res.send('This api route is not implemented yet.');
