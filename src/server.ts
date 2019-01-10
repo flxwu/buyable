1141e2a323fbf5a21106b5a82dc586ae9686f105
Felix Wu <mail@felixwu.dev>
2019-01-10 18:10:13 +0100

    build: add certificates for HTTPS on localhost

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 require('dotenv').config();
+import https from 'https';
+import fs from 'fs';
+
+const privateKey = fs.readFileSync('localhost-certificate-key.pem');
+const certificate = fs.readFileSync('localhost-certificate.pem');
+
 import app from './app';
 
 const port = process.env.PORT || 5000;
-const server = app.listen(port, () =>
-  console.log('Server running at port %d | MODE: %s', port, app.get('env'))
-);
+
+const server = https
+  .createServer(
+    {
+      key: privateKey,
+      cert: certificate
+    },
+    app
+  )
+  .listen(port, () =>
+    console.log('Server running at port %d | MODE: %s', port, app.get('env'))
+  );
 
 export default server;
