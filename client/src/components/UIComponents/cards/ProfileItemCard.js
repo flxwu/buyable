a26dd2b66b3a82c3aba5a574a00ef27a1bf313ee
avrecum <avrecum@gmail.com>
2019-01-15 19:43:14 +0100

    add profile item card

---
+++ client/src/components/UIComponents/cards/ProfileItemCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from 'grommet';
+import styled from 'styled-components';
+const Card = ({ name, description, imageURL }) => {
+  return <Box />;
+};
+
+const ProfileItemCard = styled(Card)``;
+
+export default ProfileItemCard;
