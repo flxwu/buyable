b3a971ac353bd67174ee60d020845acfc33ecf4b
Felix Wu <mail@felixwu.dev>
2019-01-15 19:42:06 +0100

    fix: component restructure

--- client/src/components/newProductModal/NewProductForm.js
+++ client/src/components/newProductModal/NewProductForm.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Box } from 'grommet';
 import styled from 'styled-components';
 
-import TextInputField from '../form/TextInputField';
-import ImageUploadField from '../form/ImageUploadField';
+import TextInputField from '../UIComponents/forms/TextInputField';
+import ImageUploadField from '../UIComponents/forms/ImageUploadField';
 
 class NewProductForm extends Component {
   render() {
     return (
       <FormContainer>
-        <TextInputField label="Product" placeholder="e.g. yodeling cucumber"/>
+        <TextInputField label="Product" placeholder="e.g. yodeling cucumber" />
         <TextInputField label="Price" placeholder="e.g. 300" />
         <ImageUploadField label="Product Image" />
       </FormContainer>
@@ -18,7 +18,7 @@ class NewProductForm extends Component {
 }
 
 const FormContainer = styled(Box)`
-	min-width: 80%;
+  min-width: 80%;
   max-width: 80%;
   margin: 15px 0;
 `;
