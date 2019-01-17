966f34c93496f422037c2cefe3c41ea6e65ab0a6
Felix Wu <mail@flxwu.com>
2019-01-17 10:40:24 +0100

    style(client): link

--- client/src/components/UIComponents/Link.js
+++ client/src/components/UIComponents/Link.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Text } from 'grommet';
 import styled from 'styled-components';
 
 import { COLORS } from '../../helpers/constants';
 
 const Link = props => <LinkText {...props}>{props.children}</LinkText>;
 
-const LinkText = styled(Text)`
+const LinkText = styled.span`
   &:hover {
     color: ${COLORS.primary};
   }
+  color: black;
   cursor: pointer;
+  text-decoration: none;
 `;
 
 export default Link;
