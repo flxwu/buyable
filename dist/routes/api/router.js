3823f91defe1f759f5c317cd42828273847f6b3b
Felix Wu <mail@felixwu.dev>
2019-01-17 10:41:25 +0100

    feat(api): add timeline router

--- dist/routes/api/router.js
+++ dist/routes/api/router.js
@@ -7,12 +7,14 @@ const express_1 = require("express");
 const itemRouter_1 = __importDefault(require("./item/itemRouter"));
 const userRouter_1 = __importDefault(require("./user/userRouter"));
 const groupRouter_1 = __importDefault(require("./group/groupRouter"));
+const timelineRouter_1 = __importDefault(require("./timeline/timelineRouter"));
 const authRouter_1 = __importDefault(require("./authRouter"));
 const experimentalRouter_1 = __importDefault(require("./experimentalRouter"));
 const router = express_1.Router();
 router.use('/item', itemRouter_1.default);
 router.use('/user', userRouter_1.default);
 router.use('/group', groupRouter_1.default);
+router.use('/timeline', timelineRouter_1.default);
 router.use('/auth', authRouter_1.default);
 router.use('/beta', experimentalRouter_1.default);
 router.use('/', (req, res) => {
