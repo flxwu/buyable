acdaa59adb9f524f1febda47a61dc233ffee0152
Felix Wu <mail@flxwu.com>
2019-02-16 21:03:11 +0100

    feat(client,checkout): checkout form draft

--- client/src/components/routes/checkout/index.js
+++ client/src/components/routes/checkout/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import { Box } from 'grommet';
+import React, { useState } from 'react';
+import { Elements, StripeProvider } from 'react-stripe-elements';
+import CheckoutForm from './CheckoutForm';
 
 const CheckoutRoute = () => {
-  return <Box fill />;
+  const [elementFontSize, setElementFontSize] = useState(
+    window.innerWidth < 450 ? '14px' : '18px'
+  );
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 450 && elementFontSize !== '14px') {
+      setElementFontSize('14px');
+    } else if (window.innerWidth >= 450 && elementFontSize !== '18px') {
+      setElementFontSize('18px');
+    }
+  });
+
+  return (
+    <StripeProvider apiKey="pk_test_QwmQOyzYXsW6k7sLGaILPtJ5">
+      <div className="example">
+        <h1>
+          Checkout from{' '}
+          <span style={{ fontWeight: 500, fontSize: 42, fontFamily: 'Caveat' }}>
+            Buyable
+          </span>
+        </h1>
+        <Elements>
+          <CheckoutForm fontSize={elementFontSize} />
+        </Elements>
+      </div>
+    </StripeProvider>
+  );
 };
 
 export default CheckoutRoute;
