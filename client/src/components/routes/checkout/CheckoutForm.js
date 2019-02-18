ebefec9df99b195f018fc3629722f0cdaf80aa3a
Felix Wu <mail@flxwu.com>
2019-02-18 17:12:10 +0100

    feat(client,checkout): styled-components checkout form styling

--- client/src/components/routes/checkout/CheckoutForm.js
+++ client/src/components/routes/checkout/CheckoutForm.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import {
   CardNumberElement,
   CardExpiryElement,
   CardCVCElement,
-  PostalCodeElement,
   injectStripe
 } from 'react-stripe-elements';
-
-// import '../../../style/checkout.css';
 
 const handleBlur = () => {
   console.log('[blur]');
@@ -57,50 +56,100 @@ class CheckoutForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <Label>
           Card number
-          <CardNumberElement
+          <CNE
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
             onReady={handleReady}
             {...createOptions(this.props.fontSize)}
           />
-        </label>
-        <label>
+        </Label>
+        <Label>
           Expiration date
-          <CardExpiryElement
+          <CEE
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
             onReady={handleReady}
             {...createOptions(this.props.fontSize)}
           />
-        </label>
-        <label>
+        </Label>
+        <Label>
           CVC
-          <CardCVCElement
+          <CVC
             onBlur={handleBlur}
             onChange={handleChange}
             onFocus={handleFocus}
             onReady={handleReady}
             {...createOptions(this.props.fontSize)}
           />
-        </label>
-        <label>
-          Postal code
-          <PostalCodeElement
-            onBlur={handleBlur}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onReady={handleReady}
-            {...createOptions(this.props.fontSize)}
-          />
-        </label>
-        <button>Pay</button>
+        </Label>
+        <Button>Pay</Button>
       </form>
     );
   }
 }
+
+const Label = styled.label`
+  color: #6b7c93;
+  font-weight: 300;
+  letter-spacing: 0.025em;
+`;
+
+const Button = styled.button`
+  white-space: nowrap;
+  border: 0;
+  outline: 0;
+  display: inline-block;
+  height: 40px;
+  line-height: 40px;
+  padding: 0 14px;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  color: #fff;
+  border-radius: 4px;
+  font-size: 15px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 0.025em;
+  background-color: #6772e5;
+  text-decoration: none;
+  -webkit-transition: all 150ms ease;
+  transition: all 150ms ease;
+  margin-top: 10px;
+  &:hover {
+    color: #fff;
+    cursor: pointer;
+    background-color: #7795f8;
+    transform: translateY(-1px);
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+  }
+`;
+
+const inputsStyles = `
+  display: block;
+  margin: 10px 0 20px 0;
+  max-width: 500px;
+  padding: 10px 14px;
+  font-size: 1em;
+  box-shadow: rgba(50, 50, 93, 0.14902) 0px 1px 3px,
+    rgba(0, 0, 0, 0.0196078) 0px 1px 0px;
+  font-family: 'Raleway', sans-serif;
+  border: 0;
+  outline: 0;
+  border-radius: 4px;
+  background: white;
+`;
+
+const CNE = styled(CardNumberElement)`
+  ${inputsStyles}
+`;
+const CVC = styled(CardCVCElement)`
+  ${inputsStyles}
+`;
+const CEE = styled(CardExpiryElement)`
+  ${inputsStyles}
+`;
 
 export default injectStripe(CheckoutForm);
