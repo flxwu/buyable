aad42a5a8b22e9d49edddd2d072bf11d763f7c69
Felix Wu <mail@flxwu.com>
2019-02-18 17:12:25 +0100

    feat(client,checkout): add back button

--- client/src/components/routes/checkout/index.js
+++ client/src/components/routes/checkout/index.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
 
-const CheckoutRoute = () => {
+import { FormPreviousLink } from 'grommet-icons';
+
+const CheckoutRoute = ({ history }) => {
   const [elementFontSize, setElementFontSize] = useState(
     window.innerWidth < 450 ? '14px' : '18px'
   );
+
+  const [backButtonHovered, setHovered] = useState(false);
 
   window.addEventListener('resize', () => {
     if (window.innerWidth < 450 && elementFontSize !== '14px') {
@@ -15,18 +19,45 @@ const CheckoutRoute = () => {
     }
   });
 
+  const backButtonStyle = {
+    position: 'absolute',
+    width: '3rem',
+    height: '3rem',
+    left: 0,
+    marginTop: '2rem'
+  };
+  const backButtonStyleHover = {
+    position: 'absolute',
+    width: '3rem',
+    height: '3rem',
+    left: 0,
+    marginTop: '2rem',
+    cursor: 'pointer',
+    transform: 'translateX(-2px)'
+  };
+
   return (
     <StripeProvider apiKey="pk_test_QwmQOyzYXsW6k7sLGaILPtJ5">
-      <div className="example">
-        <h1>
-          Checkout from{' '}
-          <span style={{ fontWeight: 500, fontSize: 42, fontFamily: 'Caveat' }}>
-            Buyable
-          </span>
-        </h1>
-        <Elements>
-          <CheckoutForm fontSize={elementFontSize} />
-        </Elements>
+      <div style={{ padding: '0 8rem', position: 'relative' }}>
+        <FormPreviousLink
+          color="#6772e5"
+          style={backButtonHovered ? backButtonStyleHover : backButtonStyle}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+          onClick={() => history.push('/')}
+        />
+        <div>
+          <h1>
+            Checkout from{' '}
+            <span
+              style={{ fontWeight: 500, fontSize: 42, fontFamily: 'Caveat' }}>
+              Buyable
+            </span>
+          </h1>
+          <Elements>
+            <CheckoutForm fontSize={elementFontSize} />
+          </Elements>
+        </div>
       </div>
     </StripeProvider>
   );
