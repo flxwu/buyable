862fcbf408c537c06a0b70c3606846051f0684ad
avrecum <avrecum@gmail.com>
2019-03-26 19:06:36 +0100

    feat(server): https switch in .env HTTPS var

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,44 @@
 require('dotenv').config();
 import https from 'https';
+import http from 'http';
 import fs from 'fs';
 
 const privateKey = fs.readFileSync('localhost-certificate-key.pem');
 const certificate = fs.readFileSync('localhost-certificate.pem');
+const useHttps = Number(process.env.HTTPS) ? true : false;
 
-import app from './app';
-
+import configureApp from './app';
+let server;
 const port = process.env.PORT || 5000;
-
-const server = https
-  .createServer(
-    {
-      key: privateKey,
-      cert: certificate
-    },
-    app
-  )
-  .listen(port, () =>
-    console.log('Server running at port %d | MODE: %s', port, app.get('env'))
-  );
+const startServer = async () => {
+  const app = await configureApp();
+  server = useHttps
+    ? https
+        .createServer(
+          {
+            key: privateKey,
+            cert: certificate
+          },
+          app
+        )
+        .listen(port, () =>
+          console.log(
+            'Server running at port %d | MODE: %s',
+            port,
+            app.get('env')
+          )
+        )
+    : http
+        .createServer(app)
+        .listen(port, () =>
+          console.log(
+            'Server running at port %d | MODE: %s',
+            port,
+            app.get('env'),
+            '\nWARNING: HTTPS DISABLED'
+          )
+        );
+};
+startServer();
 
 export default server;
