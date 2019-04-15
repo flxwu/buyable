76fd984a69782d2f683936997f47f2b3be49d3cb
avrecum <avrecum@gmail.com>
2019-04-15 09:13:46 +0200

    fix(frondend, search): spread all props to SearchItem

--- client/src/components/routes/search/SearchResults.js
+++ client/src/components/routes/search/SearchResults.js
@@ -1,64 +1,80 @@
 import React from 'react';
 
-import SearchItem from './SearchItem'
+import SearchItem from './SearchItem';
 
 import axios from 'axios';
 import querySearch from 'stringquery';
 import { withRouter } from 'react-router-dom';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { getCurrentSearchQueryObject } from '../../../redux/selectors';
 
-import {updateSearchOptions }from '../../../redux/actions/search'
+import { updateSearchOptions } from '../../../redux/actions/search';
 
-class SearchResults extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {query: null, results: null, loading: false, error: null}
+class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { query: null, results: null, loading: false, error: null };
+  }
+  async getSearchResults({ name, minPrice, maxPrice }) {
+    this.setState(state => ({ ...state, loading: true }));
+    try {
+      return {
+        result: await axios.get('/api/search', {
+          params: { name, minPrice, maxPrice }
+        })
+      };
+    } catch (error) {
+      return { error };
     }
-    async getSearchResults({name, minPrice, maxPrice}){
-        this.setState(state=>({...state, loading: true}));
-        try{
-            return {result:await axios.get('/api/search', {params:{name, minPrice, maxPrice}})};
-        }catch(error){
-            return {error};
-        }
+  }
+  async componentDidMount() {
+    const query = querySearch(this.props.location.search);
+    if (query && query !== this.props.query) {
+      this.props.updateSearchObject(query);
+      const { result, error } = await this.getSearchResults(query);
+      if (result) {
+        this.setState(state => ({
+          ...state,
+          results: result.data.results,
+          loading: false,
+          error: null
+        }));
+      } else {
+        console.log(JSON.parse(JSON.stringify(error.response.status)));
+        this.setState(state => ({
+          ...state,
+          results: null,
+          loading: false,
+          error: {
+            status: error.response.status,
+            statusText: error.response.statusText
+          }
+        }));
+      }
     }
-    async componentDidMount(){
-        const query = querySearch(this.props.location.search);
-        if(query && query !== this.props.query){
-            this.props.updateSearchObject(query);
-            const {result, error} = await this.getSearchResults(query);
-            if(result){
-                this.setState(state=>({
-                    ...state,
-                    results: result.data.results,
-                    loading: false,
-                    error: null
-                }));
-            }else{
-                console.log(JSON.parse(JSON.stringify(error.response.status)));
-                this.setState(state=>({
-                    ...state,
-                    results: null,
-                    loading: false,
-                    error: {status: error.response.status, statusText: error.response.statusText}
-                }))
-            }
-        }
-    }
-    render(){
-        const {results, loading, error} = this.state;
-        return results ?
-        this.state.results.map((item, i)=>(<SearchItem 
-            key={i} name={item.name} description={item.description} price={item.price} amount={item.amount}
-            />))
-        :loading?<div>Loading..</div>:error?<div>{JSON.stringify(error)}</div>:<div>type something</div>
-    }
+  }
+  render() {
+    const { results, loading, error } = this.state;
+    return results ? (
+      this.state.results.map((item, i) => <SearchItem key={i} {...item} />)
+    ) : loading ? (
+      <div>Loading..</div>
+    ) : error ? (
+      <div>{JSON.stringify(error)}</div>
+    ) : (
+      <div>type something</div>
+    );
+  }
 }
-const mapStateToProps = state=> ({
-    query: getCurrentSearchQueryObject(state)
-})
-const mapDispatchToProps = ()=>({
-    updateSearchObject: updateSearchOptions
-})
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchResults));
+const mapStateToProps = state => ({
+  query: getCurrentSearchQueryObject(state)
+});
+const mapDispatchToProps = () => ({
+  updateSearchObject: updateSearchOptions
+});
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(SearchResults)
+);
