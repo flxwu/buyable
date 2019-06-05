3942759244a2612ddc56bb42dba19d84996db318
avrecum <avrecum@gmail.com>
2019-06-06 00:15:21 +0200

    feat(api, item): added update and delete, validation and fixes

--- src/routes/api/item/itemRouter.ts
+++ src/routes/api/item/itemRouter.ts
@@ -2,13 +2,21 @@ import { Router } from 'express';
 
 const router: Router = Router();
 import Controller from './itemController';
+import {
+  newPOSTValidators,
+  GETValidators,
+  UPDATEValidators,
+  DELETEValidators
+} from './itemValidators';
 /**
  * POST New Product
  */
 
 const itemController = new Controller();
 
-router.post('/new', itemController.newPOST);
-router.get('/', itemController.GET);
+router.post('/new', newPOSTValidators, itemController.newPOST);
+router.get('/', GETValidators, itemController.GET);
+router.patch('/', UPDATEValidators, itemController.UPDATE);
+router.delete('/', DELETEValidators, itemController.DELETE);
 
 export default router;
